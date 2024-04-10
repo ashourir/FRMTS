e7a3fab73163215337efaf4e6658fb2095ba1d43
Rosa Ashouri <rashouri01@mynbcc.ca>
2024-04-10 19:17:52 -0300

    Merge two tabs, remove Scanned doc with remove doc

--- removeDoc_proc.php
+++ removeDoc_proc.php
@@ -1,16 +1,57 @@
 <?php
+
 include("CLASSES/Document.php");
 
-(array) $docIds = $_POST["docCheck"];
+// Retrieve the document IDs from the POST request
+$docIds = isset($_POST["docCheck"]) ? $_POST["docCheck"] : [];
 
-for ($i=0; $i<count($docIds); $i++) {
-    $success = Document::MakeUnavailableById($docIds[$i]);
+
+// Function to remove a directory and its contents
+function removeDirectory($dir) {
+    if (!is_dir($dir)) {
+        return false;
+    }
+
+    $files = array_diff(scandir($dir), array('.', '..'));
+
+    foreach ($files as $file) {
+        $path = $dir . '/' . $file;
+        if (is_dir($path)) {
+            removeDirectory($path);
+        } else {
+            unlink($path);
+        }
+    }
+
+    return rmdir($dir);
+}
+
+
+foreach ($docIds as $docId) {
+    
+    $dirName = "./UPLOADS/" . "doc" . $docId;
+
+    // Attempt to remove the directory and its contents
+    if (removeDirectory($dirName)) {
+        $msg = "Scanned document removed successfully.";
+    } else {
+        $msg = "Failed to remove Scanned document.";
+    }
+
+    // Attempt to make the document unavailable by ID
+    $success = Document::MakeUnavailableById($docId);
     if (!$success) {
         $msg = "Something went wrong. Please try again.";
-    } 
-    else {
-        if (count($docIds)==1) $msg = "Document removed.";
-        else $msg = "Documents removed.";
+    } else {
+        if (count($docIds) == 1) {
+            $msg .= "Document removed.";
+        } else {
+            $msg .= "Documents removed.";
+        }
     }
 }
-header("location:employee.php?msg=$msg");
+
+
+header("location: employee.php?msg=$msg");
+?>
+
